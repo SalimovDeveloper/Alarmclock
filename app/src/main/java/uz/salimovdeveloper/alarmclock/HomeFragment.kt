ee469a0aadbfb2e3d698686184b4ee955cd579a6
ACER <salimovdeveloper@gmail.com>
2023-01-03 21:48:16 -0600

    commit

---
+++ app/src/main/java/uz/salimovdeveloper/alarmclock/HomeFragment.kt
@@ -0,0 +1,20 @@
+package uz.salimovdeveloper.alarmclock
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import uz.salimovdeveloper.alarmclock.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+
+        return binding.root
+    }
+}
--- app/src/main/java/uz/salimovdeveloper/alarmclock/MainActivity.kt
+++ app/src/main/java/uz/salimovdeveloper/alarmclock/MainActivity.kt
@@ -1,11 +1,48 @@
 package uz.salimovdeveloper.alarmclock
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import uz.salimovdeveloper.alarmclock.databinding.ActivityMainBinding
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // Set the input and output filenames
+        val inputFilename = "input.mp4"
+        val outputFilename = "output.mp4"
+
+
+
+// Set the filter options
+        val filterOptions = "-vf curves=r='0/255':g='0/255':b='0/255'"
+
+
+
+// Build the ffmpeg command
+        val command = arrayOf("ffmpeg", "-i", inputFilename, filterOptions, "-y", outputFilename)
+
+
+
+// Call ffmpeg
+//        val process = ProcessBuilder(*command)
+//            .redirectErrorStream(true)
+//            .start()
+
+
+
+// Print the ffmpeg output
+//        BufferedReader(InputStreamReader(process.inputStream)).use { reader ->
+//            reader.lines().forEach {
+
+            }
+//        }
+//    }
 }
